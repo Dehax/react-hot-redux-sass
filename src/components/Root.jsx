73b0f9cff88fdbb55238c1a2a53bbeaea77d2c3d
Denis Kubashevskiy <dehax12@gmail.com>
2018-10-10 11:41:05 +0300

    Fix HMR for Redux store

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,20 +1,15 @@
 import App from 'components/App/App'
 import React from 'react'
 import {hot} from 'react-hot-loader'
-import {Provider} from 'react-redux'
 import BrowserRouter from 'react-router-dom/BrowserRouter'
-
-import store from 'store'
 
 
 class Root extends React.Component {
   render () {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import 'components/index.css'
 import Root from 'components/Root'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import store from 'store'
 
 
 axios.defaults.baseURL = process.env.REACT_APP_HOST_API
 
-ReactDOM.render(<Root />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <Root />
+  </Provider>,
+  document.getElementById('root'),
+)
